18a4c3dffcf3e5a71c0542be60de370124f9ec59
Alex Galuza <0.chto.a.cho.0@gmail.com>
2023-12-28 18:27:02 +0300

    lesson1/task2

--- Desktop/lesson1c/lesson1/Task2/Program.cs
+++ Desktop/lesson1c/lesson1/Task2/Program.cs
@@ -12,8 +12,15 @@ double sum3 = d1 + i1;
 
 int product1 = i1 * i2;
 long product2 = l1* i1;
-double product3 = d1 * i1;
+double product3 = i1 * d1;
 
 Console.WriteLine(product1);
 Console.WriteLine(product2);
 Console.WriteLine(product3);
+
+int quotient = i1 / i2;
+Console.WriteLine(quotient);
+
+d1 = 6;
+double quotient2 = i1 / d1;
+Console.WriteLine(quotient2);

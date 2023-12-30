2aee1d053e06f02c65098dd9cf73840439f08210
Alex Galuza <0.chto.a.cho.0@gmail.com>
2023-12-30 13:35:22 +0300

    lesson1/task6

---
+++ Desktop/lesson1c/lesson1/task6/Program.cs
@@ -0,0 +1,59 @@
+﻿// Console.Clear();
+// Console.Write("Введите число ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// if (b * b == a)
+//     Console.WriteLine("yes");
+// else
+//     Console.WriteLine("no");
+
+
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int i = n * (-1);
+// while (i <= n)
+// {
+//     Console.Write(i + "\t");
+//     i += 1; // i = i + 1;
+// }
+
+
+// Console.Clear();
+// Console.Write("Введите число ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int n1 = n / 100;
+// int n3 = n % 10;
+
+// Console.WriteLine(n1 + n3);
+
+// Console.Write("Введите число ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine()); ;
+
+// if (firstNumber > secondNumber)
+// {
+//     Console.WriteLine("Первое число больше");
+// }
+// if (firstNumber < secondNumber)
+// {
+//     Console.WriteLine("второе число больше");
+// }
+// if (firstNumber == secondNumber)
+// {
+//     Console.WriteLine("числа равны");
+// }
+
+int number = 6;
+ int i = 1;
+while (i <= number)
+{   if(number % 2 == 0)
+   {Console.Write(i);
+    Console.Write(' ');
+    i = i + 1;}
+    number = number - 1}

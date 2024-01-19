484cdb4a7ea315cc5d9b3802b24bb21bf5dc9cd4
Alex Galuza <0.chto.a.cho.0@gmail.com>
2024-01-19 21:11:20 +0300

    Funk.lesson1: task1

---
+++ Desktop/lesson_massive/lesson2/Funk.lesson1/task1/Program.cs
@@ -0,0 +1,9 @@
+﻿double CalculatFormula(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int denomenator = c + d;
+    double result = numenator / denomenator;
+    return result;
+}
+double result = CalculatFormula(1, 2, 3, 4);
+Console.WriteLine(result);

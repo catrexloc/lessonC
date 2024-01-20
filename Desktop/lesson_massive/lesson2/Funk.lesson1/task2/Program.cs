230813917917f07ecc2c889d3d1696c87907e3c3
Alex Galuza <0.chto.a.cho.0@gmail.com>
2024-01-20 12:48:06 +0300

    lessonFunk: task2

---
+++ Desktop/lesson_massive/lesson2/Funk.lesson1/task2/Program.cs
@@ -0,0 +1,12 @@
+﻿void PrintSquares(int n)
+{
+    int i = 1;
+    while(i<=n)
+    {
+        Console.Write($"{i*i} ");
+        i++;
+    }
+}
+PrintSquares(5);
+PrintSquares(10);
+PrintSquares(15);

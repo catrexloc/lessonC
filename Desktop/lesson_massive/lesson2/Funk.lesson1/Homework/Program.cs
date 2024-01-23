7b18628f373d5940cec88bd21b509eb475910cc4
Alex Galuza <0.chto.a.cho.0@gmail.com>
2024-01-23 15:59:15 +0300

    алгоритмы

---
+++ Desktop/lesson_massive/lesson2/Funk.lesson1/Homework/Program.cs
@@ -0,0 +1,42 @@
+﻿void FillArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(1, 100);
+}
+
+void GetSumOfElements(int[] array)
+{
+int sum = 0;
+for(int i = 0; i< array.Length; i++)
+    {
+        sum += array[i];
+    }
+    Console.Write(sum);
+}
+
+void GetProductOfElements(int[] array)
+{
+    int product = 1;
+for(int i = 0; i< array.Length; i++)
+    {
+        product = product * array[i];
+    }
+    Console.Write(product);
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+
+FillArray(array);
+Console.WriteLine(string.Join(", ",array));
+
+GetSumOfElements(array);
+
+GetProductOfElements(array);
+
+
+
+
+
---
+++ Desktop/lesson_massive/lesson3/task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Линейный алгоритм
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int n = int.Parse(Console.ReadLine()!), result = 0;
+// // int n = ConvertToInt32(Console.ReadLine())
+// for(int i = 1; i <= n; i++)
+//     result += i;
+// Console.WriteLine($"Сумма чисел от 1 до {n} равна {result}");
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма чисел от 1 до {n} равна {(1 + n) / 2.0 * n}");

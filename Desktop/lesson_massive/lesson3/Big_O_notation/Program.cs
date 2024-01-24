44fdffeacd2f549a1d70c46001c87e369b76aa3e
Alex Galuza <0.chto.a.cho.0@gmail.com>
2024-01-24 15:13:15 +0300

    Big O Notation

---
+++ Desktop/lesson_massive/lesson3/Big_O_notation/Program.cs
@@ -0,0 +1,37 @@
+﻿// int n = 5;
+// int[] array = new int[n];
+// for (int i = 0; i < n; i++)
+//     array[i] = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("[" + string.Join(" ,", array) + "]");
+// Console.WriteLine(array[3]);
+// //Сложность алгоритма O(1) 
+// // O notation. Алгоритм будет работать за O  до одного.
+// int sum = 0;
+// for (int i = 0; i < n; i++)
+//     sum += array[i];
+// Console.WriteLine(sum);
+//O(n)
+
+//O(n^2)
+int n = Convert.ToInt32(Console.ReadLine());
+int[, ] matrix = new int [n, n];
+for (int i =0;  i < n ; i++)
+{
+    for(int j = i; j < n; j++)
+    {
+    matrix[i,j] = (i + 1) * (j + 1);
+    matrix[j,i] = (i + 1) * (j + 1);
+    }
+    Console.WriteLine();
+}
+for(int i = 0; i < n; i++)
+{
+    for(int j = 0;j < n; j++)
+    {
+        Console.Write(matrix[i,j]);
+        Console.Write(" ");
+    }
+    Console.WriteLine();
+}
+    
+

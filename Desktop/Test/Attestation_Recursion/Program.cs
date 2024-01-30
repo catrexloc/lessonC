4cf0c4f93645f01c06f2cc994c1b7666f98f7cbb
Alex Galuza <0.chto.a.cho.0@gmail.com>
2024-01-30 19:06:24 +0300

    Задача 1

---
+++ Desktop/Test/Attestation_Recursion/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу,
+// которая выведет все натуральные числа в промежутке от M до N.
+// Использовать рекурсию, не использовать циклы.
+
+int rec(int m, int n)
+{
+if (m==n)
+return n;
+else 
+Console.WriteLine(m);
+return m + rec(m + 1, n);
+}
+Console.Write("Вывести числа в промежутке");
+Console.Write("от: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("до: ");
+int n = Convert.ToInt32(Console.ReadLine());
+rec(m, n);
